381ec54568327ade68dc85bf8c9f20b78dd5f728
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-13 15:24:05 +0100

    Removed assert message

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,8 +39,8 @@ class Server:
         Get the paginated pages from the dataset using index_range
         helper function
         """
-        assert page > 0, "page and page_size must be greater then zero"
-        assert page_size > 0, "page_size must be greater than zero"
+        assert page > 0
+        assert page_size > 0
         start_idx, end_idx = index_range(page, page_size)
         if end_idx > len(self.dataset()):
             return []
