09556ab0db8bcc8315a8454c0ee7a73a531a447c
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-13 15:14:14 +0100

    Implementing a simple pagination class

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" A simple pagination """
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    """ Helper function for a simple pagination """
+    start_idx = 0
+    end_idx = 0
+
+    for i in range(page):
+        start_idx = end_idx
+        start_idx += page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """
+    Server class to paginate a database of popular baby names
+    """
+    DATA_FILE = "Popular_Baby_Name.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """ Cached dataset """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def get_page(self, page: int, page_size: int = 10) -> List[List]:
+        assert int > 0 and page_size > 0, "page and page_size \
+                must be greater then zero"
+        start_idx, end_idx = index_range(page, page_size)
+        if end_idx > len(self.dataset()):
+            return []
+        get_pages = self.dataset()[start_idx: end_idx]
+        return get_pages
