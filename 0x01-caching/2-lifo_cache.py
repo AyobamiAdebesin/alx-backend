52cc3b1edc6913f06f6e05d11ac34ebee8b00e9a
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-14 14:50:31 +0100

    replaced or with an

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -12,17 +12,17 @@ class LIFOCache(BaseCaching):
 
     def put(self, key, item):
         """ Store data into the cache """
+        cache_len = len(self.cache_data)
         if key is None or item is None:
             return None
-        cache_len = len(self.cache_data)
-        if cache_len >= BaseCaching.MAX_ITEMS or key not in self.cache_data:
-            temp = list(self.cache_data.keys())[-1]
+        if cache_len >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            temp = list(self.cache_data.keys())[0]
             del self.cache_data[temp]
             print("DISCARD: {}".format(temp))
         self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an object frm the cache """
+        """ Get an object from the cache """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data[key]
