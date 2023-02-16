c123518b9d4e36f26d96890623e83fe0b7184508
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-16 01:29:09 +0100

    Refactor: optimize

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -11,14 +11,14 @@ class LRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache.
+        """Store an item in the cache.
         """
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                temp= self.cache_data.popitem(True)
+                print("DISCARD:", temp)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
