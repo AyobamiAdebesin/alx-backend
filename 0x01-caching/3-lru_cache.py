0e2f10e992f5cebbb204c2f5c230b8f4e204cbe9
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-16 01:33:14 +0100

    Refactor: optimizing

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,7 +16,7 @@ class LRUCache(BaseCaching):
         if key is None or item is None:
             return
         if key not in self.cache_data:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 temp = self.cache_data.popitem(True)
                 print("DISCARD:", temp)
             self.cache_data[key] = item
