d271e7aa25a3ecbd7f2671e6c63089c5c0f2782f
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-25 13:19:14 +0100

    config file for babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Configuration for Babel """
+from typing import List
+
+class Config(object):
+    """ Config class for Babel """
+    LANGUAGES : List[str] = ['en', 'fr']
