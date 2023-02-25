79aa6fab6bb09a2a83a4dc4eab95aa0e0f0ae2e8
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-25 15:20:44 +0100

    Created a Babel config class

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
-""" Configuration for Babel """
+""" A Basic Flask App with Babel Config """
 from typing import List
+from flask import Flask, render_template
+from flask_babel import Babel
 
-class Config(object):
-    """ Config class for Babel """
-    LANGUAGES : List[str] = ['en', 'fr']
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES : List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
