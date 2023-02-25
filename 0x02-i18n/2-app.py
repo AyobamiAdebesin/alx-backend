289ae1efabc1ee6581c81087adeed795abfed4c1
AyobamiAdebesin <froshofficial@gmail.com>
2023-02-25 15:28:56 +0100

    Implemented locale selector

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" A Basic Flask App with Babel Config """
+from typing import List
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """ Index page """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
